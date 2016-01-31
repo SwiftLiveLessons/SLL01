52c0fd5aefc58f810c8a7ab47750d798e87eb052
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2016-01-31 22:28:51 +0100

    Improves readability.

--- LiveLessons3/Lesson3_6_enums.playground/contents.swift
+++ LiveLessons3/Lesson3_6_enums.playground/contents.swift
@@ -12,17 +12,17 @@ myColor = .Yellow
 
 
 enum MacOSXNames: String {
-    case Cheetah = "10.0"
-    case Puma = "10.1"
-    case Jaguar = "10.2"
-    case Panther = "10.3"
-    case Tiger = "10.4"
-    case Leopard = "10.5"
-    case SnowLeopard = "10.6"
-    case Lion = "10.7"
+    case Cheetah      = "10.0"
+    case Puma         = "10.1"
+    case Jaguar       = "10.2"
+    case Panther      = "10.3"
+    case Tiger        = "10.4"
+    case Leopard      = "10.5"
+    case SnowLeopard  = "10.6"
+    case Lion         = "10.7"
     case MountainLion = "10.8"
-    case Mavericks = "10.9"
-    case Yosemite = "10.10"
+    case Mavericks    = "10.9"
+    case Yosemite     = "10.10"
     
     mutating func setToCheetah() {
         self = .Cheetah
