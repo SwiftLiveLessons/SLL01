dbfa5ae6564c6f1efac7f96e57c84896c3ea750b
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2016-02-01 23:45:01 +0100

    Updates lesson to Swift 2.

--- LiveLessons3/Lesson3_9_optionals.playground/contents.swift
+++ LiveLessons3/Lesson3_9_optionals.playground/contents.swift
@@ -17,9 +17,9 @@ myclass = nil
 myclass = MyClass(value: 1337)
 
 if let unwrappedMyClass = myclass {
-    println("myclass is not nil, and value = \(unwrappedMyClass.value)")
+    print("myclass is not nil, and value = \(unwrappedMyClass.value)")
 } else {
-    println("myclass is nil")
+    print("myclass is nil")
 }
 
 
@@ -27,38 +27,38 @@ if let unwrappedMyClass = myclass {
 var a: Int? = 7, b: Int? = 4, c: Int? = 6
 if let ua = a, ub = b, uc = c {
     let sum = ua + ub + uc
-    println("\n\nmultiple if-let binding sum is \(sum)\n\n")
+    print("\n\nmultiple if-let binding sum is \(sum)\n\n")
 }
 
 // multiple if-let binding on one line with where clause
 if let ua = a, ub = b, uc = c where ua > ub {
     let sum = ua + ub + uc
-    println("multiple if-let binding with where clause sum is \(sum)\n\n")
+    print("multiple if-let binding with where clause sum is \(sum)\n\n")
 } else {
-    println("this failed")
+    print("this failed")
 }
 
 // multiple if-let binding on one line with where clause with leading boolean conditional
 if myclass != nil, let ua = a, ub = b, uc = c where ua > ub {
     let sum = ua + ub + uc
-    println("multiple if-let binding with where clause sum is \(sum)\n\n")
+    print("multiple if-let binding with where clause sum is \(sum)\n\n")
 }
 
 
 // using switch statements to unwrap
 switch myclass {
 case .Some(let unwrappedMyClass):
-    println("in switch, myclass is not nil, and value = \(unwrappedMyClass.value)")
+    print("in switch, myclass is not nil, and value = \(unwrappedMyClass.value)")
 case .None:
-    println("myclass is nil")
+    print("myclass is nil")
 }
 
 
 // nil coalescing
-println("myclass?.value = \(myclass?.value)")
+print("myclass?.value = \(myclass?.value)")
 
 let newValue = myclass?.value ?? 0
-println("newValue = \(newValue)")
+print("newValue = \(newValue)")
 
 
 
